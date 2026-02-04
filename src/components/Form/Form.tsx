80d6f02ef69dce5d3ec59c3b24f45de067eedf65
MartaKMB <muszkamarta@interia.pl>
2026-02-04 16:43:39 +0100

    feat(api): basic fetch for color name

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -8,6 +8,15 @@ interface FormProps {
 export function Form({ onFormSubmit }: FormProps) {
   const [inputName, setInputName] = useState('');
   const [inputColor, setInputColor] = useState('');
+  const [colorName, setColorName] = useState('');
+
+  function handleLoadColor(hex: string) {
+    const hexFetch = hex.slice(1);
+    fetch(`https://www.thecolorapi.com/id?hex=${hexFetch}`)
+      .then((res) => res.json())
+      .then((res) => setColorName(res.name.value));
+  }
+
   return (
     <section className={styles.container}>
       <h2>create your hero</h2>
@@ -26,12 +35,18 @@ export function Form({ onFormSubmit }: FormProps) {
           className={styles.input}
         />
         <label>choose color</label>
-        <input
-          value={inputColor}
-          onChange={(e) => setInputColor(e.target.value)}
-          type='color'
-          className={styles.inputColor}
-        />
+        <div className={styles.inputContainer}>
+          <input
+            value={inputColor}
+            onChange={(e) => {
+              handleLoadColor(e.target.value);
+              setInputColor(e.target.value);
+            }}
+            type='color'
+            className={styles.inputColor}
+          />
+          <p>{colorName}</p>
+        </div>
         <button className={styles.button}>create!</button>
       </form>
     </section>
